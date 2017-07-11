35338c4741951c01a01e0add11ff46aab53da104
Raphael Regnier <raphael.regnier@hotmail.fr>
2017-07-11 16:11:54 -0400

    added description to seed and show

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,11 @@ Book.destroy_all
 Order.destroy_all
 User.destroy_all
 
-
-
 10.times do
-
 user1 = User.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name,email: Faker::Internet.email, password: Faker::Internet.password)
-book1 = Book.create(title: Faker::Book.title, price: Faker::Number.between(1, 30), category: Faker::Book.genre, author: Faker::Book.author, user: user1)
-book2 = Book.create(title: Faker::Book.title, price: Faker::Number.between(1, 30), category: Faker::Book.genre, author: Faker::Book.author, user: user1)
-book3 = Book.create(title: Faker::Book.title, price: Faker::Number.between(1, 30), category: Faker::Book.genre, author: Faker::Book.author, user: user1)
+book1 = Book.create(title: Faker::Book.title, price: Faker::Number.between(1, 30), category: Faker::Book.genre, author: Faker::Book.author, description: Faker::Lorem.sentence(3, true) ,user: user1)
+book2 = Book.create(title: Faker::Book.title, price: Faker::Number.between(1, 30), category: Faker::Book.genre, author: Faker::Book.author, description: Faker::Lorem.sentence(3, true) ,user: user1)
+book3 = Book.create(title: Faker::Book.title, price: Faker::Number.between(1, 30), category: Faker::Book.genre, author: Faker::Book.author, description: Faker::Lorem.sentence(3, true) ,user: user1)
 
 
 order1 = Order.create(start_time: Faker::Date.between(Date.today, 1.week.from_now) ,end_time: Faker::Date.between(1.week.from_now, 2.week.from_now), book: book1, user: user1)
