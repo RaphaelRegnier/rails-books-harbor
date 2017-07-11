d197e81d604a8928fa546c5f965314807591a11a
Hong Park <hongp26@gmail.com>
2017-07-11 12:32:55 -0400

    make the basic view of users and create the basic sign-up sign-in action

---
+++ db/migrate/20170711162638_add_first_name_last_name_pick_up_address_to_users.rb
@@ -0,0 +1,7 @@
+class AddFirstNameLastNamePickUpAddressToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :pickup_address, :string
+  end
+end
