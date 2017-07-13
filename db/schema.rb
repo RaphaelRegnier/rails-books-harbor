d7d47bd1d4878f6898295e74b092eccc8bb2c257
Hong Park <hongp26@gmail.com>
2017-07-13 15:57:34 -0400

    fix the geocode

--- db/schema.rb
+++ db/schema.rb
@@ -39,18 +39,7 @@ ActiveRecord::Schema.define(version: 20170713162421) do
     t.string   "author"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.float    "latitude"
-    t.float    "longitude"
     t.index ["user_id"], name: "index_books_on_user_id", using: :btree
-  end
-
-  create_table "flats", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.float    "latitude"
-    t.float    "longitude"
   end
 
   create_table "orders", force: :cascade do |t|
