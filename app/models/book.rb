8d37fcf32cffb548b86b046b4a81d22fe80f45a3
Hong Park <hongp26@gmail.com>
2017-07-13 14:23:41 -0400

    geocode moves from books to users and bundle install of gmaps4rails

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,6 @@ class Book < ApplicationRecord
   has_many :orders, dependent: :destroy
   validates :title, :price, :category, presence: true
   has_attachment :photo
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
 
   def self.search(search)
     where("title ILIKE ? OR author ILIKE ?", "%#{search}%", "%#{search}%")
