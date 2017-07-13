cb875ef4edb83487f68b3c7b4597574c76e147ee
Sanghoo Oh <shoh04190915@gmail.com>
2017-07-13 15:00:04 -0400

    finished profile show I think.

--- db/schema.rb
+++ db/schema.rb
@@ -49,8 +49,6 @@ ActiveRecord::Schema.define(version: 20170713162420) do
     t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float    "latitude"
-    t.float    "longitude"
   end
 
   create_table "orders", force: :cascade do |t|
