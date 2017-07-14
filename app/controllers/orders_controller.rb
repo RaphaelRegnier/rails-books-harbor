87a5f3226111058cc53e53b45d091306c30728d9
Sanghoo Oh <shoh04190915@gmail.com>
2017-07-14 08:39:33 -0400

    some weird duplication

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,21 +2,15 @@ class OrdersController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-
-
-    # @orders = Order.where(book_id: @book.id).where("(DATEDIFF(order.start_time, ?) * DATEDIFF(?, order.end_time)) >= 0", params[:order][:start_time].to_date, params[:order][:end_time].to_date)
-
-
-    # if @orders.count > 0
-    #   render show
-    # else
-      @order = Order.new(order_params)
-      @order.book = @book
-      @order.user = current_user
-      @order.save
-      redirect_to current_user
-    # end
-
+    @order = Order.new(order_params)
+    @order.book = @book
+    @order.user = current_user
+    if book_is_available(@order.start_time, @order.end_time, @book)
+       @order.save
+        redirect_to current_user
+    else
+      redirect_to book_path(@book)
+    end
   end
 
   def destroy
@@ -29,5 +23,17 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:start_time, :end_time)
+  end
+
+  def book_is_available(start_time, end_time, book)
+    orders = Order.where(book_id: book.id)
+    date_range = start_time..end_time
+    orders.each do |order|
+      if order.start_time === date_range || order.end_time === date_range
+        return book_available = false
+      else
+       return book_available = true
+      end
+    end
   end
 end
